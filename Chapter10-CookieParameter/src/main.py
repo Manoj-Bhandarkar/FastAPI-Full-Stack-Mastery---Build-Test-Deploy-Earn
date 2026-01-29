2e200e35e9f51788d6f89e6148f7862ec4361976
manoj-bhandakar <developer.manojbhandarkar@gmail.com>
2026-01-29 20:12:10 +0530

    Section 5 : Cookie Parameters

---
+++ Chapter10-CookieParameter/src/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI, Cookie
+from typing import Annotated
+app = FastAPI()
+
+#--------------------------------------
+## Cookie Parameters
+@app.get("/products/recommendations")
+async def get_recommendations(session_id: Annotated[str | None, Cookie()] = None):
+  if session_id:
+    return {"message": f"Recommendations for session {session_id}", "session_id": session_id}
+  return {"message": "No session ID provided, showing default recommendations"}
+
+# curl -H "Cookie: session_id=abc123" http://127.0.0.1:8000/products/recommendations => {"message": f"Recommendations for session abc123", "session_id": abc123}
+# curl -H  http://127.0.0.1:8000/products/recommendations   => {"message": "No session ID provided, showing default recommendations"}
+
+
