0b921fc05f2856a65d7b68781966c5c3e35fec02
manoj-bhandakar <developer.manojbhandarkar@gmail.com>
2026-02-01 19:56:24 +0530

    Section 7 : Response Return Type - List of return, Not accept Extra field in return and Return only Object of Pydantic or json

--- Chapter13-ResponseModel/src/main.py
+++ Chapter13-ResponseModel/src/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 #---------------------------------
 # Response Model
 #---------------------------------
-class Product_Response(BaseModel):
+class Product(BaseModel):
   id: int
   name: str
   price : float
@@ -20,14 +20,40 @@ class ProductOut(BaseModel):
 #-------------------------------------
 ## without response_model Parameter
 #-------------------------------------
-@app.get("/products10")
-async def get_products10():
+@app.get("/products1")
+async def get_products1():
   return {"id": 1, "name": "Moto E", "price": 33.44, "stock": 5}
 
 #-------------------------------------
 # with response_model Parameter
 #-------------------------------------
-@app.get("/products11", response_model=Product_Response)
-async def get_products11():
-  return {"id": 1, "name": "Moto E", "price": 33.44, "stock": 5}
+@app.get("/products2", response_model=Product)
+async def get_products2():
+  return {"id": 1,  "price": 33.44, "stock": 5}
 
+#-------------------------------------
+#  response_model Parameter with List of return
+#-------------------------------------
+@app.get("/products3", response_model=List[Product])
+async def get_products():
+  return [
+       {"id": 1, "name": "Moto E", "price": 33.44, "stock": 5},
+       {"id": 2, "name": "Redmi 4", "price": 55.33, "stock": 7}
+    ]
+
+#-------------------------------------
+#  response_model Parameter - Not accept Extra field
+#-------------------------------------
+@app.get("/products4", response_model=List[Product])
+async def get_products4():
+  return [
+       {"id": 1, "name": "Moto E", "price": 33.44, "stock": 5, "description": "Hello Desc1"},
+       {"id": 2, "name": "Redmi 4", "price": 55.33, "stock": 7, "description": "Hello Desc2"}
+    ]
+
+#-------------------------------------
+# Return only Object - Pydantic or json
+#-------------------------------------
+@app.post("/products5", response_model=Product)
+async def create_product5(product: Product):
+  return {"id": 2, "name": "Redmi 4", "price": 55.33, "stock": 7}
