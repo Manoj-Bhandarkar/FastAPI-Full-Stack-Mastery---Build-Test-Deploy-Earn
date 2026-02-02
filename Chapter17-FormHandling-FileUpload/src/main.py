b17b94cf625c3b7df1de8ae3fc9d1d934ff59811
manoj-bhandakar <developer.manojbhandarkar@gmail.com>
2026-02-02 17:32:28 +0530

    Section 8 : File Handling - Upload File

---
+++ Chapter17-FormHandling-FileUpload/src/main.py
@@ -0,0 +1,71 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import HTMLResponse
+from typing import Annotated
+import os
+import uuid
+import shutil
+
+app = FastAPI()
+
+
+# HTML form for testing
+@app.get("/", response_class=HTMLResponse)
+async def main():
+    return """
+    <html>
+        <body>
+            <h2>Single File Upload (bytes)</h2>
+            <form action="/files/" enctype="multipart/form-data" method="post">
+                <input name="file" type="file">
+                <input type="submit" value="Upload">
+            </form>
+            <h2>Single File Upload (UploadFile)</h2>
+            <form action="/uploadfile/" enctype="multipart/form-data" method="post">
+                <input name="file" type="file">
+                <input type="submit" value="Upload">
+            </form>
+        </body>
+    </html>
+    """
+#--------------------------
+## Return File Length not store
+#--------------------------
+@app.post("/files1/")
+async def create_file1(file: Annotated[bytes | None, File()] = None):
+    if not file:
+        return {"message": "No file sent"}
+    return {"file size": len(file)}
+
+
+#--------------------------
+## Create File - File store with folder - selected file name not stored
+#--------------------------
+@app.post("/files2/")
+async def create_file2(file: Annotated[bytes | None, File()] = None):
+    if not file:
+        return {"message": "No file sent"}
+    
+    filename = f"{uuid.uuid4()}.bin"
+    save_path = f"uploads/{filename}"
+
+    os.makedirs("uploads", exist_ok=True)
+
+    with open(save_path, "wb") as buffer:
+        buffer.write(file)
+
+    return {"file size": len(file)}
+
+
+#--------------------------
+## UploadFile - File store with folder - selected file name is stored with original name
+#--------------------------
+@app.post("/uploadfile/")
+async def create_upload_file(file: Annotated[UploadFile | None, File()] = None):
+    if not file:
+        return {"message": "No upload file sent"}
+    
+    save_path = f"uploads/{file.filename}"
+    os.makedirs("uploads", exist_ok=True)
+    with open(save_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    return {"filename": file.filename, "content_type": file.content_type}
