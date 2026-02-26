7db76e53e565845ae54c19d8710ebade497736f6
manoj-bhandakar <developer.manojbhandarkar@gmail.com>
2026-02-26 16:07:14 +0530

    Section 17 : SQLModel with FastAPI - Database Operations

--- Chapter44-FastAPI-with-SQLModel/app/main.py
+++ Chapter44-FastAPI-with-SQLModel/app/main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
-from app.user.models import User
-from app.product.models import Product
 from app.db.config import create_tables
 from contextlib import asynccontextmanager
+from app.user.services import *
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -10,3 +9,13 @@ async def lifespan(app: FastAPI):
   yield
 
 app = FastAPI(lifespan=lifespan)
+
+@app.post("/user")
+def user_create(new_user: dict):
+  user = create_user(name=new_user["name"], email=new_user["email"])
+  return user
+
+@app.get("/users")
+def all_users():
+  users = get_all_users()
+  return users
