d0c619a05761705b458500e533ba81764055e6d3
manoj-bhandakar <developer.manojbhandarkar@gmail.com>
2026-03-02 16:42:50 +0530

    Section 19 : Middleware - Separate Middleware file

--- Chapter46-Middleware/app/main.py
+++ Chapter46-Middleware/app/main.py
@@ -1,22 +1,11 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
+from app.middlewares import my_first_middleware, my_second_middleware
 
 app = FastAPI()
 
-# First Middleware
-@app.middleware("http")
-async def my_first_middleware(request: Request, call_next):
-  print("1st Middleware: Before processing the request")
-  response = await call_next(request)
-  print("1st Middleware: After processing the request, before returning response")
-  return response
+app.middleware("http")(my_second_middleware)
+app.middleware("http")(my_first_middleware)
 
-# Second Middleware
-@app.middleware("http")
-async def my_second_middleware(request: Request, call_next):
-  print("2nd Middleware: Before processing the request")
-  response = await call_next(request)
-  print("2nd Middleware: After processing the request, before returning response")
-  return response
 
 @app.get("/users")
 async def get_users():
