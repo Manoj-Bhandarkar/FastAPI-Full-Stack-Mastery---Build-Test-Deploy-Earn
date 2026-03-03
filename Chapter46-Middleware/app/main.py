936d164564efabb981aeee99e635e2f9558dec91
manoj-bhandakar <developer.manojbhandarkar@gmail.com>
2026-03-03 07:10:59 +0530

    Section 19 : Middleware - Built0in Middleware

--- Chapter46-Middleware/app/main.py
+++ Chapter46-Middleware/app/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
 from app.middlewares import users_only_middleware, product_only_middleware, my_middleware
+from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
 
 app = FastAPI()
+
+# built-in middleware
+app.add_middleware(HTTPSRedirectMiddleware)
+# app.add_middleware(TrustedHostMiddleware, allowed_hosts=["localhost", "127.0.0.1"])
 
 app.middleware("http")(product_only_middleware)
 app.middleware("http")(users_only_middleware)
