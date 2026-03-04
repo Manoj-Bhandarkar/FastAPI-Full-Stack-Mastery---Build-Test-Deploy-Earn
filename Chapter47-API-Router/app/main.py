0aff928846aa51b6532db413e32a22b320f7b02d
manoj-bhandakar <developer.manojbhandarkar@gmail.com>
2026-03-04 19:51:21 +0530

    Section 20 : API Router - Tags for grouping on Swagger Docs

--- Chapter47-API-Router/app/main.py
+++ Chapter47-API-Router/app/main.py
@@ -4,30 +4,12 @@ from app.product.routers import router as product_routers
 
 app = FastAPI()
 
+# app.include_router(user_routers, tags=["users"])
+# app.include_router(product_routers, tags=["products"])
+
 app.include_router(user_routers)
 app.include_router(product_routers)
 
 @app.get("/")
 async def root():
   return {"data": "Root"}
-
-
-# @app.get("/users")
-# async def get_all_users():
-#   return {"data": "All Users"}
-
-# @app.get("/users/me")
-# async def get_current_user():
-#   return {"data": "Current User"}
-
-# @app.get("/users/{user_id}")
-# async def get_single_user(user_id: int):
-#   return {"data": "Single User"}
-
-# @app.get("/products")
-# async def get_all_products():
-#   return {"data": "All Products"}
-
-# @app.get("/products/{product_id}")
-# async def get_single_product(product_id: int):
-#   return {"data": "Single Product"}
--- Chapter47-API-Router/app/user/routers.py
+++ Chapter47-API-Router/app/user/routers.py
@@ -2,14 +2,28 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-@router.get("/users")
+@router.get("/users", tags=["users"])
 async def get_all_users():
   return {"data": "All Users"}
 
-@router.get("/users/me")
+@router.get("/users/me", tags=["users"])
 async def get_current_user():
   return {"data": "Current User"}
 
-@router.get("/users/{user_id}")
+@router.get("/users/{user_id}", tags=["custom"])
 async def get_single_user(user_id: int):
   return {"data": "Single User"}
+
+# router = APIRouter(tags=["users"])
+
+# @router.get("/users")
+# async def get_all_users():
+#   return {"data": "All Users"}
+
+# @router.get("/users/me")
+# async def get_current_user():
+#   return {"data": "Current User"}
+
+# @router.get("/users/{user_id}", tags=["custom"])
+# async def get_single_user(user_id: int):
+#   return {"data": "Single User"}
