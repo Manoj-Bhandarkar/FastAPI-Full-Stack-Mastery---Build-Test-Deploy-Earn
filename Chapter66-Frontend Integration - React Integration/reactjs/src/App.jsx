ac8b0bcdbc79c7b07f47fe706361e3553058b4d8
Manoj-Bhandarkar <developer.manojbhandarkar@gmail.com.com>
2026-03-26 21:35:07 +0530

    Section 25 : Frontend Integration - React Integration - Setup

---
+++ Chapter66-Frontend Integration - React Integration/reactjs/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import ProductList from './components/ProductList';
+
+function App() {
+
+  return (
+    <>
+      <div className="App">
+      
+      </div>
+    </>
+  )
+}
+
+export default App
